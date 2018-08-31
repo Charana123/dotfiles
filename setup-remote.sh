faf3af8d18b87e962c50c6e2c4d62f4d6c94e4cd
Charana <charananandasena@yahoo.com>
2018-08-31 11:45:54 +0100

    extend bashrc: configure git, git credentials, aliases

--- setup-remote.sh
+++ setup-remote.sh
@@ -7,3 +7,14 @@ sudo cp ./.vimrc /root/.vimrc
 # Copy .screenrc
 cp ./.screenrc ~/.screenrc
 sudo cp ./.screenrc /root/.screenrc
+
+echo "
+alias vi=\"vim\"
+alias top=\"top -o cpu\"
+" > ~/.bashrc
+
+echo "
+git config --global user.name \"Charana Nandasena\"
+git config --global user.email \"charana@stackhpc.com\"
+git config credential.helper cache
+" > ~/.bashrc
